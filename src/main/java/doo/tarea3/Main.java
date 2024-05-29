cbef4395b7213d3d6dc300c90016da0b0741e0a3
Mario Andres Salgado Jaque <marisalgado2023@udec.cl>
2024-05-29 14:17:21 -0400

    Creando clase Ventana con lo indicado en CANVAS y modificando la clase Main a lo que indica enunciado

--- src/main/java/doo/tarea3/Main.java
+++ src/main/java/doo/tarea3/Main.java
@@ -1,63 +1,11 @@
 package doo.tarea3;
 
 /**
- * Este es un código que simula un expendedor de bebidas y dulces, creado para la clase POO (Programacion Orientada a Objetos)
- * @author Santiago Díaz, Mario Salgado & Luis Martinez
+ * Clase main crea la ventana donde se mostrará el proyecto
+ * @author Mario Salgado
  */
 public class Main {
-    /**
-     *
-     * @param args es un array que recibe datos de la command line (no se usa en este caso)
-     * @throws NoHayProductoException es una excepcion que permite a la maquina detectar cuando el stock de producto = 0
-     * @throws PagoInsuficienteExcepcion una excepcion que permite saber cuando el dinero ingresado es menos al precio del producto
-     * @throws PagoIncorrectoException cuando no se pudo concretar la transaccion.
-     */
-    public static void main(String[] args) throws NoHayProductoException,PagoInsuficienteExcepcion,PagoIncorrectoException{
-
-
-        int numProductos = 1;
-        int prodnum = 5;
-        Expendedor exp = new Expendedor(numProductos);
-
-        Moneda m = new Moneda1000();
-
-        Comprador compa = null;
-        m = new Moneda1000();
-        try {
-            compa = new Comprador(m, prodnum, exp);
-        } catch (NoHayProductoException e) {
-            System.out.println((e.getMessage()));
-        } catch (PagoInsuficienteExcepcion e) {
-            System.out.println((e.getMessage()));
-        } catch (PagoIncorrectoException e) {
-            System.out.println((e.getMessage()));
-        }
-        System.out.println(compa.queCompraste());
-        System.out.println(compa.cuantoVuelto());
-
-        Comprador compa2 = null;
-        m = null;
-        try {
-            compa2 = new Comprador(m, prodnum, exp);
-        } catch (NoHayProductoException e) {
-            System.out.println((e.getMessage()));
-        } catch (PagoInsuficienteExcepcion e) {
-            System.out.println((e.getMessage()));
-        } catch (PagoIncorrectoException e) {
-            System.out.println((e.getMessage()));
-        }
-
-        Comprador compa3 = null;
-        prodnum=1;
-        m = new Moneda100();
-        try {
-            compa3 = new Comprador(m, prodnum, exp);
-        } catch (NoHayProductoException e) {
-            System.out.println((e.getMessage()));
-        } catch (PagoInsuficienteExcepcion e) {
-            System.out.println((e.getMessage()));
-        } catch (PagoIncorrectoException e) {
-            System.out.println((e.getMessage()));
-        }
+    public static void main(String[] args) {
+        Ventana v = new Ventana();
     }
 }
---
+++ src/main/java/doo/tarea3/Ventana.java
@@ -0,0 +1,16 @@
+package doo.tarea3;
+import java.awt.*;
+import javax.swing.*;
+
+public class Ventana extends JFrame { // como extiende a un JFrame, es un JFrame, pero con algunos agregados
+    // propiedades que puedes agregar aquí para que compongan a Ventana. Ventana "es un" JFrame
+    public Ventana(){
+        super();
+        this.setLayout(new BorderLayout()); // aunque tiene Layout por defecto, se define uno que es mas práctico
+        // auto configuración: puedes poner aquí sentencias para inicializar las propiedades nuevas y heredades de JFrame
+        this.setTitle("Expendedor y comprador");
+        this.setDefaultCloseOperation(EXIT_ON_CLOSE); // permite que la X para cerrar la ventana esté habilitada para finalizar la ejecución
+        this.setSize(1000,800);
+        this.setVisible(true);
+    }
+}
